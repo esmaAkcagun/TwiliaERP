232f760f21c0f1a5be7295899e70e26c52a9b12a
tahsinSlamanca <gokmentahsin@gmail.com>
2020-07-26 03:54:10 +0200

    second

--- src/test/java/com/twiliaerp/utilities/Driver.java
+++ src/test/java/com/twiliaerp/utilities/Driver.java
@@ -18,10 +18,7 @@ import java.net.URL;
 public class Driver {
     private Driver() {
     }
-    // InheritableThreadLocal  --> this is like a container, bag, pool.
-    // in this pool we can have separate objects for each thread
-    // for each thread, in InheritableThreadLocal we can have separate object for that thread
-    // driver class will provide separate web driver object per thread
+    git commit -m "first commit"
     private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
     public static WebDriver get() {
         //if this thread doesn't have driver - create it and add to pool
